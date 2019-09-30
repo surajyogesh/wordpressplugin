816b8814be880f95729cb6b4eacf01cebb61d11f
Suraj Shah <surajyogesh.shah@my.jcu.edu.au>
2019-10-01 01:01:28 +1000

    1.1.1

---
+++ suraj-first-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Suraj First Plugin
+* Plugin URI: localhost/firstsite/
+* Description: This is the very first plugin I ever created.
+* Version: 1.0
+* Author: Suraj Shah
+* Author URI: localhost/firstsite/
+**/
